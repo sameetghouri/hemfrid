60e1223732025191502e7d6061dea8a4e39920ee
Sameet <sameetghouri@yahoo.com>
2023-10-23 22:44:49 +0500

    modify

--- src/Components/pages/Home/Sections/TimeforSimpler.js
+++ src/Components/pages/Home/Sections/TimeforSimpler.js
@@ -1,13 +1,13 @@
 
 const TimeforSimple=()=>{
 
-    return (
-<div className="h-screen bg-[#eae8e1] overflow-clip font-['Roboto']">
+return (
+<div className="  bg-[#eae8e1] overflow-clip font-['Roboto']">
 
-<div className="flex  overflow-hidden">
+<div className="flex flex-col lg:flex-row lg:items-stretch items-center ">
 {/* first content */}
-    <div className="">
-    <div className=" my-32 ml-20 text-black text-5xl font-medium font-['Young Serif']    ">
+    
+<div className=" my-20 px-4 lg:px-20  text-black text-5xl font-medium font-['Young Serif']    ">
 
 <p>Time for a simpler</p>
 
@@ -26,32 +26,17 @@ const TimeforSimple=()=>{
 <p className="text-center text-white ">CHOOSE SERVICES</p>
 
 <p className="text-center text-4xl  self-end  absolute end-0  mr-4"><svg height="20" viewBox="0 0 9 15" width="20" class="csw9N_C5 fiwAtSA" style={{stroke:"#fff"}}   ><path d="m.958.995 6.51 6.51-6 6" fill="none" stroke-width="2"></path></svg></p>
-
-
-
 </div>
-   
+</div>       
 
-</div>
-        
-    </div>
 
 
 {/* second content */}
-
-
-
-<div className=" 2   ml-16 w-[47vw] absolute end-0 overflow-hidden">
+<div className=" lg:ml-auto w-[47vw]  overflow-hidden">
 <img src="https://images.ctfassets.net/37vwfxlcawdb/2xjwGOztqiIgJqeJJNjUO6/0f3f6a98bf16671ceb817a6545c33460/Kok-spis-kastrull-1-1.jpg?q=50&fm=jpg&w=1200" className=" w-[100vw] " alt="pic_image" />
-        
-    </div>
-
-
-
-
 </div>
 
-
+</div>
 </div>
 
     );
--- src/Components/pages/Home/Sections/cleaning_to_loc.js
+++ src/Components/pages/Home/Sections/cleaning_to_loc.js
@@ -21,7 +21,7 @@ return (
 <div className="lg:mr-20  mr-0">
     <div class=" flex items-start  relative">
     <a href="/Home_clean">
-  <img src="https://images.ctfassets.net/37vwfxlcawdb/3cql75pe3saYJjlsv3UhsV/8d82e3a560237967b03cc8953d824d1f/Hemfrid_Piktogram_Veckosta__dning_64x64px-01.svg" class="h-12 inline-block shake" alt="" />
+  <img src="https://images.ctfassets.net/37vwfxlcawdb/3cql75pe3saYJjlsv3UhsV/8d82e3a560237967b03cc8953d824d1f/Hemfrid_Piktogram_Veckosta__dning_64x64px-01.svg" class="h-12 inline-block hover:shake" alt="" />
 </a>
 
 
@@ -35,7 +35,7 @@ return (
 <div>
 <div class=" flex items-start  relative">    
 <a href="/Home_clean">    
-        <img  className=" inline-block  h-12" src="https://images.ctfassets.net/37vwfxlcawdb/1i6rvbHEd43Wz2cGJnavev/d719f7b2e4236a51c168ac0180ae8028/Hemfrid_Piktogram_Tra__dga__rdshja__lp_64x64px-01.svg" class="h-12 inline-block shake" alt=""  />
+        <img  className=" inline-block  h-12" src="https://images.ctfassets.net/37vwfxlcawdb/1i6rvbHEd43Wz2cGJnavev/d719f7b2e4236a51c168ac0180ae8028/Hemfrid_Piktogram_Tra__dga__rdshja__lp_64x64px-01.svg" class="h-12 inline-block hover:shake" alt=""  />
         </a>
         <p className="text-2xl inline-block  ml-8">Gardening help</p>
         <a href="/Home_clean">
@@ -57,7 +57,7 @@ return (
     <div className="lg:mr-20 mr-0">
 <div className="flex items-start relative">
 <a href="/Deep_Clean">
-    <img  className="h-12 inline-block" src="https://images.ctfassets.net/37vwfxlcawdb/40uG0fMTP0zEqxAaNXwlSH/93887b204bd4f0da127e8ddabd5cc082/Hemfrid_Piktogram_Storsta__dning_64x64px-01.svg" class="h-12 inline-block shake" alt=""  />
+    <img  className="h-12 inline-block" src="https://images.ctfassets.net/37vwfxlcawdb/40uG0fMTP0zEqxAaNXwlSH/93887b204bd4f0da127e8ddabd5cc082/Hemfrid_Piktogram_Storsta__dning_64x64px-01.svg" class="h-12 inline-block hover:shake" alt=""  />
 </a>
     <p className="text-2xl inline-block ml-8">Deep Cleaning</p>
     <a href="/Deep_Clean">
@@ -69,7 +69,7 @@ return (
     <div className="">
 
         <div className="flex items-end relative">
-            <img  className="h-12 inline-block" src="https://images.ctfassets.net/37vwfxlcawdb/2Ermbt31Q34AyWAlucVeIq/cc358344d63fdf3ad2dcb54cde61c1fd/Hemfrid_Piktogram_Enklare_hantverk_64x64px-01.svg" class="h-12 inline-block shake" alt=""  />
+            <img  className="h-12 inline-block" src="https://images.ctfassets.net/37vwfxlcawdb/2Ermbt31Q34AyWAlucVeIq/cc358344d63fdf3ad2dcb54cde61c1fd/Hemfrid_Piktogram_Enklare_hantverk_64x64px-01.svg" class="h-12 inline-block hover:shake" alt=""  />
             <p className="text-2xl inline-block ml-8">General handyman</p> 
             <p className="text-center text-4xl   absolute bottom-3 end-0"><svg height="24" viewBox="0 0 9 15" width="24" class="csw9N_C5 fiwAtSA" style={{stroke:"#1e1e1e"}}><path d="m.958.995 6.51 6.51-6 6" fill="none" stroke-width="2"></path></svg></p>
         </div>
@@ -84,7 +84,7 @@ return (
             
 <div className="flex items-start relative">
 <a href="/Window_clean">
-    <img  className="h-12 inline-block" src="https://images.ctfassets.net/37vwfxlcawdb/1NZbAfmuZTsGAuLez0utEC/dba8be94dd3c2ad48c39dde3296aea63/Hemfrid_Piktogram_Fo__nsterputs_64x64px-01.svg" class="h-12 inline-block shake" alt=""  />
+    <img  className="h-12 inline-block" src="https://images.ctfassets.net/37vwfxlcawdb/1NZbAfmuZTsGAuLez0utEC/dba8be94dd3c2ad48c39dde3296aea63/Hemfrid_Piktogram_Fo__nsterputs_64x64px-01.svg" class="h-12 inline-block hover:shake" alt=""  />
     </a>
     <p className="inline-block text-2xl ml-8">Window Cleaning</p>
     <a href="/Window_clean"></a>
@@ -101,7 +101,7 @@ return (
          
         <div>
             <div className="flex items-start relative flex-wrap">
-                <img  className="h-12 inline-block" src="https://images.ctfassets.net/37vwfxlcawdb/5e2SSlRP2Oxtwh7V8TMlBU/61d589d79881b8faef21103b039c2263/Hemfrid_Piktogram_Flytthja__lp_64x64px-01.svg" class="h-12 inline-block shake" alt=""  />
+                <img  className="h-12 inline-block" src="https://images.ctfassets.net/37vwfxlcawdb/5e2SSlRP2Oxtwh7V8TMlBU/61d589d79881b8faef21103b039c2263/Hemfrid_Piktogram_Flytthja__lp_64x64px-01.svg" class="h-12 inline-block hover:shake" alt=""  />
             
                 <p className="text-2xl inline-block ml-8">Moving Services</p> 
                 <p className="text-center text-4xl top-2  absolute end-0"><svg height="24" viewBox="0 0 9 15" width="24" class="csw9N_C5 fiwAtSA" style={{stroke:"#1e1e1e"}}><path d="m.958.995 6.51 6.51-6 6" fill="none" stroke-width="2"></path></svg></p>
@@ -117,7 +117,7 @@ return (
 
             <div className="lg:mr-20 mr-0">
                 <div className="flex items-start relative">
-                    <img  className="h-12 inline-block" src="https://images.ctfassets.net/37vwfxlcawdb/4ITtpVcwdKriQeffVlbkUi/aabe4336229ca25210ebef3992be5db3/Hemfrid_Piktogram_A__tervinning_64x64px-01.svg" class="h-12 inline-block shake" alt=""  />
+                    <img  className="h-12 inline-block" src="https://images.ctfassets.net/37vwfxlcawdb/4ITtpVcwdKriQeffVlbkUi/aabe4336229ca25210ebef3992be5db3/Hemfrid_Piktogram_A__tervinning_64x64px-01.svg" class="h-12 inline-block hover:shake" alt=""  />
                     <p className="text-2xl inline-block ml-8 ">Recycling</p>   
                     <p className="text-center text-4xl   absolute end-0 top-2 cursor-pointer"><svg height="24" viewBox="0 0 9 15" width="24" class="csw9N_C5 fiwAtSA" style={{stroke:"#1e1e1e"}}><path d="m.958.995 6.51 6.51-6 6" fill="none" stroke-width="2"></path></svg></p> 
                 </div>
@@ -128,7 +128,7 @@ return (
             <div>
                 <div className="flex items-start relative">
                 <a href="/Moving_Home">
-                    <img  className="h-12 inline-block" src="https://images.ctfassets.net/37vwfxlcawdb/1LqmJeTBZmPEm6caJ7dUVs/422c34555c78a536c8c06da196931282/Hemfrid_Piktogram_Flyttsta__dning_64x64px-01.svg" class="h-12 inline-block shake" alt=""  />
+                    <img  className="h-12 inline-block" src="https://images.ctfassets.net/37vwfxlcawdb/1LqmJeTBZmPEm6caJ7dUVs/422c34555c78a536c8c06da196931282/Hemfrid_Piktogram_Flyttsta__dning_64x64px-01.svg" class="h-12 inline-block hover:shake" alt=""  />
                     </a>
                     <p className="text-2xl inline-block ml-8">Moving out Cleaning</p>
                     <a href="/Moving_Home">
