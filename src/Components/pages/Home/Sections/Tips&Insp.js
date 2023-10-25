8ed4108db6ec35503d1ea8320e09332dd93fd87f
codezone10 <codezone108@gmail.com>
2023-10-25 15:23:24 -0700

    bug resolve on tips and inspiration

--- src/Components/pages/Home/Sections/Tips&Insp.js
+++ src/Components/pages/Home/Sections/Tips&Insp.js
@@ -23,6 +23,108 @@ const Tipsinsp=()=>{
 <div className="   mt-14 mx-24  h-fit   flex  overflow-hidden pb-24  ">
 <div className="flex flex-col justify-between items-start mr-7">
 
+<div className="group relative overflow-hidden h-[65vh] bg-[#fff] w-fit top-4 transition-all duration-800 ease-in-out hover:h-[80vh] rounded-3xl">
+  <div className="relative">
+    <img
+      src="https://images.ctfassets.net/37vwfxlcawdb/1TfE1UU6y8uJw1qSaSGma6/2ff488f042edf4fe6a7ce60a33c3abca/Startsida.jpg?q=50&fm=jpg&w=800"
+      className="w-[700px] h-80"
+      alt="Pic"
+    />
+  </div>
+
+  <div className="overflow-hidden h-0 transition-all duration-800 ease-in-out group-hover:h-[30vh] delay-200 p-8">
+  <div className="flex flex-col items-center">
+  <p className="text-xl font-bold">CLEANING</p>
+  <p className="border-black border-b w-20 text-center mt-4"></p>
+</div>
+
+    <div className="flex items-center justify-center flex-col p-8">
+      <p className="text-3xl font-medium mt-2">Get the patio ready for </p>
+      <p className="text-3xl font-medium mt-2">Winter</p>
+    </div>
+  </div>
+</div>
+
+
+
+<div className="group relative overflow-hidden h-[65vh] bg-[#fff] w-fit top-4 transition-all duration-800 ease-in-out hover:h-[80vh] rounded-3xl">
+  <div className="relative">
+    <img
+      src="https://images.ctfassets.net/37vwfxlcawdb/1TfE1UU6y8uJw1qSaSGma6/2ff488f042edf4fe6a7ce60a33c3abca/Startsida.jpg?q=50&fm=jpg&w=800"
+      className="w-[700px] h-80"
+      alt="Pic"
+    />
+  </div>
+
+  <div className="overflow-hidden h-0 transition-all duration-800 ease-in-out group-hover:h-[30vh] delay-200 p-8">
+  <div className="flex flex-col items-center">
+  <p className="text-xl font-bold">CLEANING ORGANIZATION</p>
+  <p className="border-black border-b w-20 text-center mt-4"></p>
+</div>
+
+    <div className="flex items-center justify-center flex-col p-8">
+    <p className="text-3xl font-medium mt-2">More fluency in everyday</p>
+      <p  className="text-3xl font-medium mt-2" >life-5 simple tips</p>
+    </div>
+  </div>
+</div>
+
+
+
+
+
+<div className="group relative overflow-hidden h-[65vh] bg-[#fff] w-fit top-4 transition-all duration-800 ease-in-out hover:h-[80vh] rounded-3xl">
+  <div className="relative">
+    <img
+      src="https://images.ctfassets.net/37vwfxlcawdb/4l8RDAxaEze1utGrkmIWcl/fd7bd29adbf662cdd8182b695e1973d9/Sta_d-.jpg?q=50&fm=jpg&w=800"
+      className="w-[700px] h-80"
+      alt="Pic"
+    />
+  </div>
+
+  <div className="overflow-hidden h-0 transition-all duration-800 ease-in-out group-hover:h-[40vh] delay-200 p-8">
+  <div className="flex flex-col items-center">
+  <p className="text-xl font-bold">CLEANING</p>
+  <p className="border-black border-b w-20 text-center mt-4"></p>
+</div>
+
+    <div className="flex items-center justify-center flex-col p-8">
+    <p className="text-3xl font-medium mt-2">Dansih hygge at home -</p>
+          <p  className="text-3xl font-medium mt-2" >small details that make a</p>
+          <p  className="text-3xl font-medium mt-2" >difference</p>
+    </div>
+  </div>
+</div>
+</div>
+
+
+
+
+<div className="flex flex-col justify-between items-start ">
+
+    
+<div className="group relative overflow-hidden h-[65vh] bg-[#fff] w-fit top-4 transition-all duration-800 ease-in-out hover:h-[80vh] rounded-3xl">
+  <div className="relative">
+    <img
+      src="https://images.ctfassets.net/37vwfxlcawdb/4l8RDAxaEze1utGrkmIWcl/fd7bd29adbf662cdd8182b695e1973d9/Sta_d-.jpg?q=50&fm=jpg&w=800"
+      className="w-[700px] h-80"
+      alt="Pic"
+    />
+  </div>
+  <div className="overflow-hidden h-0 transition-all duration-800 ease-in-out group-hover:h-[30vh] delay-200 p-8">
+  <div className="flex flex-col items-center">
+  <p className="text-xl font-bold">CLEANING</p>
+  <p className="border-black border-b w-20 text-center mt-4"></p>
+</div>
+
+    <div className="flex items-center justify-center flex-col p-8">
+    <p className="text-3xl font-medium mt-2">Clean your oven quickly</p>
+          <p  className="text-3xl font-medium mt-2" >and easily</p>
+    </div>
+  </div>
+    </div>
+
+
     
     </div>
 
