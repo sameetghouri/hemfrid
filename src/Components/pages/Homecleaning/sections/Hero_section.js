323ee9b0a388ab3df5d6962eb776aa2d0eb9fec3
codezone10 <codezone108@gmail.com>
2023-10-25 09:25:41 -0700

    Home Cleaning HEro section Modifications

--- src/Components/pages/Homecleaning/sections/Hero_section.js
+++ src/Components/pages/Homecleaning/sections/Hero_section.js
@@ -10,15 +10,16 @@ const Hero =()=>{
 
 
 <div className=" z-0  w-[35%] absolute top-36 ml-20 text-white text-5xl font-medium font-['Young Serif']  lg:ml-28  xl:ml-24  2xl:ml-36">
-
+<div className="hover:text-gray-500">
     <p className="text-start">Get routine for</p>
     
-    <p class="  text-start mt-1 ">your home</p>
-    <p class="  text-start mt-1 ">cleaning</p>
+    <p class="  text-start mt-1 ">your <span className="text-yellow-300">Home <span className="text-orange-600">Cleaning</span></span></p>
+    {/* <p class="  text-start mt-1 "><span className="text-orange-400">Cleaning</span></p> */}
+    </div>
 <div className="flex justify-start w-full">
-    <input className="bg-white focus:border-none focus:outline-none  w-full text-black text-center text-lg p-3 rounded-md mt-10 placeholder-black font-bold cursor-pointer" placeholder="ZIPCODE" />
+    <input className="bg-white focus:border-none focus:outline-none w-full text-black text-center text-lg p-3 rounded-md mt-10 placeholder-black font-bold cursor-pointer bg-opacity-80 hover:shadow-lg hover:shadow-black hover:bg-white transform hover:scale-90 transition-transform delay-200 hover:placeholder-gray-500" placeholder="ENTER ZIPCODE" />
 </div>
-<div className="flex justify-center items-center  w-full bg-opacity-80 hover:shadow-lg hover:shadow-black bg-[#1e1e1e] text-lg font-bold mt-4 p-5 rounded-md">
+<div className="flex justify-center items-center  w-full bg-opacity-80 hover:shadow-lg hover:shadow-black bg-[#1e1e1e] text-lg font-bold mt-4 p-5 rounded-md hover:text-green-500 transform hover:scale-90 transition-transform delay-200">
 
    <p className="text-center text-['#fff'] ">CONTINUE</p>
    
@@ -33,15 +34,23 @@ const Hero =()=>{
    <div className="mt-5">
 
  
-    
-        <svg height="24" width="24" viewBox="0 0 24 24" color='white' class="uiyb_O3 cfwk5OWE inline-block  mr-2" style={{fill:"white"}}><g><path d="m12 0a12 12 0 1 0 0 24 12 12 0 0 0 0-24zm0 22.286a10.286 10.286 0 1 1 0-20.572 10.286 10.286 0 0 1 0 20.572z"></path><path d="m17.4 7.114-6.257 6.24-2.828-2.81a.848.848 0 1 0 -1.2 1.2l3.428 3.428a.824.824 0 0 0 1.2 0l6.857-6.858a.849.849 0 0 0 -1.2-1.2z"></path></g></svg>      
-        <p className="inline-block"> <span className="text-xl shadow-md">Right Now! 50% discount on your first invoice</span> </p>
+   <div class="flex items-center">
+  <svg height="24" width="24" viewBox="0 0 24 24" color="white" class="uiyb_O3 cfwk5OWE" style={{fill: "white"}}><g><path d="m12 0a12 12 0 1 0 0 24 12 12 0 0 0 0-24zm0 22.286a10.286 10.286 0 1 1 0-20.572 10.286 10.286 0 0 1 0 20.572z"></path><path d="m17.4 7.114-6.257 6.24-2.828-2.81a.848.848 0 1 0 -1.2 1.2l3.428 3.428a.824.824 0 0 0 1.2 0l6.857-6.858a.849.849 0 0 0 -1.2-1.2z"></path></g>
+  </svg>
+  <p class="ml-2">
+  <span className="animate-bounce text-gray-200 font-bold">Right Now!</span>
+  <span className="text-orange-200 font-bold ml-2 mr-1">50%</span>
+</p>
+<p> discount on your first invoice</p>
+
+</div>
+
         <div className="my-6 text-base text-start">
-            <p>Use the code <span className="font-bold  ">OKT50</span>  to activate the offer.</p>
+            <p>Use the code <span className="font-bold text-black text-lg">OKT50</span>  to activate the offer.</p>
         </div>  
         
-        <div className="my-6 text-lg text-start ">
-            <i>Terms:Offer valid for new customers only. Your first home <br/> cleaning must be done no later than November 30,2023. <br/>The discount cannot be combined with other promotions or offer. </i>
+        <div className="my-6 text-lg text-start hover:shake">
+            <i><span className="font-bold text-black">Terms:</span>Offer valid for new customers only. Your first home <br/> cleaning must be done no later than November 30,2023. <br/>The discount cannot be combined with other promotions or offer. </i>
         </div>
 
     </div>
